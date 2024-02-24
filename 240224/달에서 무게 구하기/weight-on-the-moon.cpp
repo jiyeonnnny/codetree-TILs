907e42d19236a9ccfa71ba70a0d91af966752f86
지연 <139419091+jiyeonnnny@users.noreply.github.com>
2024-02-24 21:25:42 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240224/달에서 무게 구하기/weight-on-the-moon.cpp
+++ 240224/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -3,6 +3,9 @@ using namespace std;
 int main() {
     int a = 13;
     double b = 0.165;
-    printf("%d * %.6f = %.6f", a,b,a*b);
+    cout<<fixed;
+    cout.precision(6);
+    cout<<a<<" * "<<b<<" = "<<a*b;
+    
     return 0;
 }
